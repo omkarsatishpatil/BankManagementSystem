baaabd27779027b715f3ebfda6e3298fc36df601
Omkar Satish Patil <omkarsatishpatil2941@gmail.com>
2024-02-26 12:08:24 +0530

    Project uploaded to github

---
+++ Login.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+class Login extends JFrame
+{
+    Login()
+    {
+        
+        setTitle("AUTOMATED TELLER MACHINE");
+        setSize(800,480);
+        setVisible(true);
+        setLocation(350,200);
+
+    }
+
+public static void main(String Arg[])
+{
+    new Login();
+}
+}
